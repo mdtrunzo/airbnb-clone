445f82fcd1dc722528691c78762e61a4c02cbdd7
Matias Daniel Trunzo <matiasdanieltrunzo@gmail.com>
2022-10-15 18:16:20 +0200

    fix

--- components/Footer.js
+++ components/Footer.js
@@ -15,7 +15,7 @@ function Footer() {
         <h5 className="font-bold">COMMUNITY</h5>
         <p>Accesibility</p>
         <p>This is not a real site</p>
-        <p>It's a pretty awesome clone</p>
+        <p>It is a pretty awesome clone</p>
         <p>Referrals Accepted</p>
         <p>MDTRUNZO</p>
       </div>
